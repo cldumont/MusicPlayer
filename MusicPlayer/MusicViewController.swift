e5f4c7be1b2fc53b062834c93d451aeb60568e60
Craig Dumont <cldumont77@comcast.net>
2018-03-05 18:42:39 -0500

    First run at a music player app

---
+++ MusicPlayer/MusicViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MusicViewController.swift
+//  MusicPlayer
+//
+//  Created by Craig Dumont on 3/5/18.
+//  Copyright © 2018 Craig Dumont. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class MusicViewController: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        playGenre(genre: sender.currentTitle!)
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+}
